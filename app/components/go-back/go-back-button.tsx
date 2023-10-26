ead3d636fd0b8207e8dee3f79e59028cbbfc4ed6
legoffchristophealexandre@gmail.com <legoffchristophealexandre@gmail.com>
2023-10-26 17:38:47 +0200

    update taouages thérapeuthiques

--- app/components/go-back/go-back-button.tsx
+++ app/components/go-back/go-back-button.tsx
@@ -1,42 +1,11 @@
 "use client";
-import React, { useEffect } from "react";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import gsap from "gsap";
+import React from "react";
 import { useRouter } from "next/navigation";
 import { FlecheRetourUrl } from "@/app/lib/svg/divers/fleche-retour-url";
 import { retourProps } from "@/app/lib/interfaces/interfaces.js";
-gsap.registerPlugin(ScrollTrigger);
 
 export const GoBackButton = ({ color, classname }: retourProps) => {
   const router = useRouter();
-  useEffect(() => {
-    setTimeout(() => {
-      if (typeof window !== "undefined") {
-        const goBackButton: any = document.querySelector("#go-back-button");
-        const sectionsWhite = gsap.utils.toArray<HTMLElement>(".white");
-        sectionsWhite.forEach((section) => {
-          gsap.to(section, {
-            scrollTrigger: {
-              start: `top-=${
-                goBackButton != null &&
-                goBackButton?.getBoundingClientRect()["y"]
-              }`,
-              end: `bottom-=${
-                goBackButton != null &&
-                goBackButton?.getBoundingClientRect()["y"]
-              }`,
-              // markers: true,
-              trigger: section,
-              toggleClass: {
-                targets: goBackButton,
-                className: "logo-white",
-              },
-            },
-          });
-        });
-      }
-    }, 2000);
-  });
 
   return (
     <div
@@ -44,7 +13,6 @@ export const GoBackButton = ({ color, classname }: retourProps) => {
       className={`${classname} cursor-pointer`}
       onClick={() => {
         router.back();
-        // router.refresh();
       }}
     >
       <FlecheRetourUrl color={color} />
--- app/tatouages-therapeutiques/page.tsx
+++ app/tatouages-therapeutiques/page.tsx
@@ -2,12 +2,16 @@
 import React from "react";
 import SectionUne from "../components/mobile/tatouages-therapeutiques/section-1";
 import SectionDeux from "../components/mobile/tatouages-therapeutiques/section-2";
+import SectionTrois from "../components/mobile/tatouages-therapeutiques/section-3";
+import SectionQuatre from "../components/mobile/tatouages-therapeutiques/section-4";
 
 const MesRealisations = () => {
   return (
     <div className="md:hidden" id="mes-Realisations">
       <SectionUne logoWhite={false} />
       <SectionDeux logoWhite />
+      <SectionTrois logoWhite />
+      <SectionQuatre logoWhite />
     </div>
   );
 };
